ccf0a537f313555c15d9732e4da7dca0e1f03fbb
Navya Gupta <navyag0509@gmail.com>
2026-02-26 13:51:54 -0500

    commit num 2

--- src/InvoiceGeneratorFrame.java
+++ src/InvoiceGeneratorFrame.java
@@ -2,14 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InvoiceGeneratorFrame extends JFrame {
-
+    Invoice thisInvoice;
     JPanel titlePnl;
     JPanel inputPnl;
+    JTextField customerNameTF;
+    JLabel customerNameLbl;
+    JTextField streetTF;
+    JLabel streetLbl;
+    JTextField cityTF;
+    JLabel cityLbl;
+    JTextField stateTF;
+    JLabel stateLbl;
+    JTextField zipCodeTF;
+    JLabel zipCodeLbl;
     JPanel invoiceDisplayPnl;
     JPanel controlPnl;
     JLabel titleLabel;
     JTextArea customerAddressLabel;
     JTextArea itemsList;
+    JButton enterLineItemBtn;
+    JButton showInvoiceBtn;
 
 
     public InvoiceGeneratorFrame()  {
@@ -26,9 +38,47 @@ public class InvoiceGeneratorFrame extends JFrame {
         titlePnl.add(titleLabel);
         add(titlePnl, BorderLayout.NORTH);
 
+        createInputPanel();
+
         setLocationRelativeTo(null); // Center the frame on the screen
         pack();
         setVisible(true);
+    }
+
+    private void createInputPanel() {
+        inputPnl = new JPanel();
+        inputPnl.setLayout(new GridLayout(5, 2));
+
+        customerNameLbl = new JLabel("Customer Name:");
+        customerNameTF = new JTextField();
+        inputPnl.add(customerNameLbl);
+        inputPnl.add(customerNameTF);
+
+        streetLbl = new JLabel("Street:");
+        streetTF = new JTextField();
+        inputPnl.add(streetLbl);
+        inputPnl.add(streetTF);
+
+        cityLbl = new JLabel("City:");
+        cityTF = new JTextField();
+        inputPnl.add(cityLbl);
+        inputPnl.add(cityTF);
+
+        stateLbl = new JLabel("State:");
+        stateTF = new JTextField();
+        inputPnl.add(stateLbl);
+        inputPnl.add(stateTF);
+
+        zipCodeLbl = new JLabel("Zip Code:");
+        zipCodeTF = new JTextField();
+        inputPnl.add(zipCodeLbl);
+        inputPnl.add(zipCodeTF);
+        enterLineItemBtn = new JButton("Enter Line Item");
+        inputPnl.add(enterLineItemBtn);
+        showInvoiceBtn = new JButton("Show Invoice");
+        inputPnl.add(showInvoiceBtn);
+
+        add(inputPnl, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
